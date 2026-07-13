6c0f0b510cdaae913b97efd5e732d64a05eee9de
Luigi Agosti <luigi@tengio.com>
2026-07-13 19:44:42 +0200

    refactor: add scrape aliases to seaportal.go to complete facade consolidation

--- seaportal.go
+++ seaportal.go
@@ -242,3 +242,57 @@ func SemanticFingerprint(content string) string {
 func ContentChanged(oldContent, newContent string) bool {
 	return engine.ContentChanged(oldContent, newContent)
 }
+
+type ScrapeOptions = engine.ScrapeOptions
+
+type ScrapeResult = engine.ScrapeResult
+
+type SiteInfo = engine.SiteInfo
+
+type PageGroup = engine.PageGroup
+
+type PageObject = engine.PageObject
+
+type PagePerformance = engine.PagePerformance
+
+type ScrapeSummary = engine.ScrapeSummary
+
+type SampleStrategy = engine.SampleStrategy
+
+type OutputFormat = engine.OutputFormat
+
+const (
+	SampleBalanced = engine.SampleBalanced
+	SampleRandom   = engine.SampleRandom
+	SamplePriority = engine.SamplePriority
+)
+
+const (
+	OutputJSON      = engine.OutputJSON
+	OutputMarkdown  = engine.OutputMarkdown
+	OutputDirectory = engine.OutputDirectory
+)
+
+const (
+	DefaultScrapeMaxPages      = engine.DefaultScrapeMaxPages
+	DefaultScrapeMaxPerPattern = engine.DefaultScrapeMaxPerPattern
+	DefaultScrapeTimeout       = engine.DefaultScrapeTimeout
+)
+
+var ErrMissingBaseURL = engine.ErrMissingBaseURL
+
+func ScrapeSite(ctx context.Context, opts *ScrapeOptions) (*ScrapeResult, error) {
+	return engine.ScrapeSite(ctx, opts)
+}
+
+func RenderScrapeJSON(res *ScrapeResult) ([]byte, error) {
+	return engine.RenderScrapeJSON(res)
+}
+
+func RenderScrapeMarkdown(res *ScrapeResult) string {
+	return engine.RenderScrapeMarkdown(res)
+}
+
+func WriteScrapeDirectory(res *ScrapeResult, dir string) ([]string, error) {
+	return engine.WriteScrapeDirectory(res, dir)
+}
